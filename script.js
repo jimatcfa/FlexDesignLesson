51e6302f82dba42202e73e93a63aeced7a112d89
Linh Pham <Lpham21@uw.edu>
2022-07-26 16:07:25 -0700

    added script.js

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 // Exercise 1: 
 // make toggle equal the item with the class 'toggle'
 // and menu equal the item with the class 'menu'
-const toggle = undefined;
-const menu = undefined;
+const toggle = document.querySelector(".toggle");
+const menu = document.querySelector(".menu");
 
 // Exercise 2:
 // makee a function called 'toggleMenu' that 
 // writes 'working' to the console
-
+function toggleMenu() {
+    if (menu.classList.contains("active")) {
+        menu.classList.remove("active");
+        toggle.querySelector("a").innerHTML = "<span class='fas fa-bars'></span>";
+    } else {
+        menu.classList.add("active");
+        toggle.querySelector("a").innerHTML = "<span class='fas fa-times'></span>";
+    }
+}
 // Exercise 3:
 // Add an event listener to the toggle element
 // to run the toggleMenu function
+toggle.addEventListener("click", toggleMenu);
